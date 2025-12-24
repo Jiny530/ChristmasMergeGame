e00ffcefa1fccca62c110e04e465cb149054e7cc
Hyejin Park <qkrgpwls1010@gmail.com>
2025-12-25 03:00:18 +0900

    게임 기본 요구사항 구현
    1. 화면 터치한 상태로 움직이면 오브젝트를 좌우로 움직일 수 있다
    2. 화면 터치 후 릴리즈 시 오브젝트가 프레임 안으로 떨어진다
    3. 같은 레벨의 두 오브젝트가 충돌 시 다음레벨의 오브젝트가 생성되고 부딪힌 오브젝트는 사라진다

---
+++ MergeObject.js
@@ -0,0 +1,69 @@
+
+class MergeObject extends Phaser.Physics.Matter.Image {
+  constructor(scene, x, y, texture, frame, options) {
+
+    const defaultOptions = {
+      isStatic: true,
+      ...options
+    };
+    
+    super(scene.matter.world, x, y, texture, frame, defaultOptions);
+    
+    scene.add.existing(this);
+    this.scene = scene;
+    
+    this.setBounce(0.8);
+    this.setMass(60);
+    this.setScale(0.2);
+
+    const fallbackRadius = Math.min(this.displayWidth, this.displayHeight) * 0.5;
+    this.setCircle(fallbackRadius, { label: 'merge-circle' });
+    
+    if (defaultOptions.isStatic) {
+      this.setStatic(true);
+    }
+
+    this.level = 1;
+    this.isReleased = false;
+    this.isMerging = false;
+
+    // collision helpers
+    this._originalCollisionMask = null;
+    this._collisionDisabled = false;
+  }
+  
+  release() {
+    this.isReleased = true;
+    if (this.body && this.body.isStatic) {
+      this.scene.matter.body.setPosition(this.body, { x: this.x, y: this.y });
+      this.scene.matter.body.setStatic(this.body, false);
+    }
+
+    if (this._collisionDisabled) {
+      this.enableCollision();
+    }
+  }
+
+  disableCollision() {
+    if (this.body && !this._collisionDisabled) {
+      this._originalCollisionMask = (this.body.collisionFilter && this.body.collisionFilter.mask != null) ? this.body.collisionFilter.mask : 0xFFFFFFFF;
+      this.body.collisionFilter.mask = 0;
+      this._collisionDisabled = true;
+    }
+  }
+
+  enableCollision() {
+    if (this.body && this._collisionDisabled) {
+      this.body.collisionFilter.mask = (this._originalCollisionMask != null) ? this._originalCollisionMask : 0xFFFFFFFF;
+      this._collisionDisabled = false;
+    }
+  }
+
+  setStatic(isStatic) {
+    if (this.body) {
+      this.scene.matter.body.setStatic(this.body, isStatic);
+    }
+  }
+
+}
+
--- main.js
+++ main.js
@@ -4,23 +4,160 @@ class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    // TODO: 이미지/사운드 에셋 로드는 여기서
+    this.load.image('gift', 'assets/images/gift.png');
+    this.load.image('candy', 'assets/images/candy.png');
+    this.load.image('cookie', 'assets/images/cookie.png');
+    this.load.image('bell', 'assets/images/bell.png');
+    this.load.image('snowman', 'assets/images/snowman.png');
+    this.load.image('snowball', 'assets/images/snowball.png');
+    this.load.image('star', 'assets/images/star.png');
+    this.load.image('frame', 'assets/images/frame.png');
   }
 
   create() {
-    // 간단한 테스트용 텍스트 (나중에 제거 가능)
-    const { width, height } = this.scale;
-    this.add
-      .text(width / 2, height / 2, "Hello Phaser!", {
-        fontFamily: "Arial",
-        fontSize: 32,
-        color: "#ffffff",
-      })
-      .setOrigin(0.5);
+
+    const frameData = this.createFrame(this.scale.width, this.scale.height);
+    
+    this.setupPhysicsBounds(frameData);
+    this.setupTouchControls(frameData);
+    this.setupMergeEvents();
+    
+    this.createNewObject(frameData);
   }
 
+  setupMergeEvents() {
+  
+    this.matter.world.on('collisionstart', (event) => {
+      event.pairs.forEach(pair => {
+        const { bodyA, bodyB } = pair;
+
+        const objectA = bodyA.gameObject;
+        const objectB = bodyB.gameObject;
+
+        if (objectA instanceof MergeObject && objectB instanceof MergeObject) {
+          if (objectA.isReleased && objectB.isReleased &&
+              objectA.level === objectB.level) 
+              {
+                this.merge(objectA, objectB);
+              }
+            }
+          
+      });
+    });
+  }
+
+  merge(objectA, objectB) {
+
+    if (objectA.isMerging || objectB.isMerging) {
+      return;
+    }
+    
+    objectA.isMerging = true;
+    objectB.isMerging = true;
+
+    const mergeX = (objectA.x + objectB.x) / 2;
+    const mergeY = (objectA.y + objectB.y) / 2;
+    
+    const newLevel = objectA.level + 1;
+
+    objectA.destroy();
+    objectB.destroy();
+    
+    const newObject = new MergeObject(this, mergeX, mergeY, 'gift', null, {
+      isStatic: false
+    });
+    
+    newObject.level = newLevel;
+    newObject.isReleased = true;
+    
+    newObject.setScale(0.2 + (newLevel - 1) * 0.05);
+  }
+
+  createFrame(ScreenWidth, ScreenHeight) {
+
+    const middleX = ScreenWidth/2;
+    const middleY = ScreenHeight/2;
+
+    const frame = this.add.image(middleX, middleY, 'frame')
+      .setOrigin(0.5, 0.5)
+      .setScale(0.8);
+    
+    const width = frame.displayWidth;
+    const height = frame.displayHeight;
+    const top = middleY - height/2;
+    const bottom = middleY + height/2;
+    const left = middleX - width/2;
+    const right = middleX + width/2;
+    
+    return {
+      frame,
+      width,
+      height,
+      middleX,
+      middleY,
+      left,
+      right,
+      top,
+      bottom
+    };
+  }
+
+  setupPhysicsBounds(frameData) {
+    this.matter.world.setBounds(frameData.left, 0, frameData.width, frameData.bottom);
+  }
+
+  createNewObject(frameData){
+    this.newObject = new MergeObject(this, frameData.middleX, frameData.top - 50, 'gift', null, {
+      isStatic: true
+    });
+
+    this.newObject.disableCollision();
+  }
+
+  setupTouchControls(frameData) {
+    this.isDragging = false;
+    this.startTouchX = 0;
+    this.startGiftX = 0;
+    
+    this.input.on('pointerdown', (pointer) => {
+      if (this.newObject && !this.newObject.isReleased && this.newObject.body && this.newObject.body.isStatic) {
+        this.isDragging = true;
+        this.startTouchX = pointer.x;
+        this.startGiftX = this.newObject.x;
+      }
+    });
+
+    this.input.on('pointermove', (pointer) => {  
+      if (this.isDragging && this.newObject && pointer.isDown) {
+        const deltaX = pointer.x - this.startTouchX;
+        const leftLimit = frameData.left + this.newObject.displayWidth/2;
+        const rightLimit = frameData.right - this.newObject.displayWidth/2;
+
+        this.newObject.x = Phaser.Math.Clamp(this.startGiftX + deltaX, leftLimit, rightLimit);
+      }
+    });
+
+    this.input.on('pointerup', () => {
+      if (this.isDragging && this.newObject && !this.newObject.isReleased) {
+        this.isDragging = false;
+        
+        this.newObject.release();
+        this.createNewObject(frameData);
+      }
+    });
+    
+    this.input.on('pointerupoutside', () => {
+      if (this.isDragging && this.newObject && !this.newObject.isReleased) {
+        this.isDragging = false;
+        
+        this.newObject.release();
+        this.createNewObject(frameData);
+      }
+    });
+
+  }
   update(time, delta) {
-    // 매 프레임마다 돌아가는 로직은 여기서
+   
   }
 }
 
@@ -28,25 +165,15 @@ const config = {
   type: Phaser.AUTO,
   parent: "game-container",
   backgroundColor: "#1d1d1d",
-
-  // 기본 해상도 (세로형 모바일 기준) - 필요시 변경
   width: 360,
   height: 640,
-
-  // 스케일 옵션: 화면 크기에 맞게 자동으로 맞춤 + 가운데 정렬
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-
-  // 물리 엔진 (필요 없으면 나중에 끌 수 있음)
   physics: {
-    default: "arcade",
-    arcade: {
-      debug: false,
-    },
+    default: "matter",
   },
-
   scene: [MainScene],
 };
 
